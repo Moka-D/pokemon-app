e7d053ff5a3b8017562ca3c4a9a9fc7282032b97
Moka-D <65670736+Moka-D@users.noreply.github.com>
2023-09-28 23:56:13 +0900

    ポケモン図鑑の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,121 @@
-import './App.css';
+import { useEffect, useRef, useState } from 'react';
 import PokemonThumbnails from './PokemonThmbnails';
+import pokemonJson from './pokemon.json';
+
+const pokemonType: { [key: string]: string } = {
+  grass: 'くさ',
+  fire: 'ほのお',
+  water: 'みず',
+  bug: 'むし',
+  normal: 'ノーマル',
+  poison: 'どく',
+  electric: 'でんき',
+  ground: 'じめん',
+  fairy: 'ようせい',
+  fighting: 'かくとう',
+  psychic: 'エスパー',
+  rock: 'いわ',
+  ice: 'こおり',
+  dragon: 'ドラゴン',
+  ghost: 'ゴースト',
+  steel: 'はがね',
+  dark: 'あく',
+};
 
 function App() {
-  const pokemon = [
-    {
-      id: 1,
-      name: 'フシギダネ',
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
-      type: 'くさ',
-    },
-    {
-      id: 2,
-      name: 'フシギソウ',
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png',
-      type: 'くさ',
-    },
-    {
-      id: 3,
-      name: 'フシギバナ',
-      image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/3.png',
-      type: 'くさ',
-    },
-  ];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [allPokemons, setAllPokemons] = useState<any[]>([]);
+  const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const refFirstRef = useRef(true);
+
+  const getAllPokemons = () => {
+    setIsLoading(true);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setUrl(data.next);
+        createPokemonObject(data.results);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+    loaded();
+  };
+
+  const loaded = () => {
+    window.addEventListener('load', () => {
+      console.log('load');
+    });
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const createPokemonObject = (results: any) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    results.forEach((pokemon: any) => {
+      const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
+      fetch(pokemonUrl)
+        .then((res) => res.json())
+        .then(async (data) => {
+          const _image = data.sprites.other['official-artwork'].front_default;
+          const _iconImage = data.sprites.other.dream_world.front_default;
+          const _type = data.types[0].type.name;
+          const japanese = await translateToJapanese(data.name, _type);
+          const newList = {
+            id: data.id,
+            name: data.name,
+            iconImage: _iconImage,
+            image: _image,
+            type: _type,
+            jpName: japanese.name,
+            jpType: japanese.type,
+          };
+          setAllPokemons((currentList) => [...currentList, newList].sort((a, b) => a.id - b.id));
+        });
+    });
+  };
+
+  const translateToJapanese = async (name: string, type: string) => {
+    const jpName = await pokemonJson.find((pokemon) => pokemon.en.toLocaleLowerCase() === name)?.ja;
+    const jpType = await pokemonType[type];
+    return { name: jpName, type: jpType };
+  };
+
+  useEffect(() => {
+    if (refFirstRef.current) {
+      refFirstRef.current = false;
+      return;
+    }
+    getAllPokemons();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className='app-container'>
       <h1>ポケモン図鑑</h1>
       <div className='pokemon-container'>
         <div className='all-container'>
-          <PokemonThumbnails
-            id={pokemon[0].id}
-            name={pokemon[0].name}
-            image={pokemon[0].image}
-            type={pokemon[0].type}
-          />
-          <PokemonThumbnails
-            id={pokemon[1].id}
-            name={pokemon[1].name}
-            image={pokemon[1].image}
-            type={pokemon[1].type}
-          />
-          <PokemonThumbnails
-            id={pokemon[2].id}
-            name={pokemon[2].name}
-            image={pokemon[2].image}
-            type={pokemon[2].type}
-          />
+          {allPokemons.map((pokemon, index) => (
+            <PokemonThumbnails
+              id={pokemon.id}
+              name={pokemon.name}
+              image={pokemon.image}
+              iconImage={pokemon.iconImage}
+              type={pokemon.type}
+              jpName={pokemon.jpName}
+              jpType={pokemon.jpType}
+              key={index}
+            />
+          ))}
         </div>
+        {isLoading ? (
+          <div className='load-more'>now loading...</div>
+        ) : (
+          <button className='load-more' onClick={getAllPokemons}>
+            もっとみる！
+          </button>
+        )}
       </div>
     </div>
   );
--- src/PokemonThmbnails.tsx
+++ src/PokemonThmbnails.tsx
@@ -1,20 +1,27 @@
+import monsterBall from './assets/monsterBall.jpg';
+
 type Props = {
   id: number;
   name: string;
+  iconImage?: string;
   image: string;
   type: string;
+  jpName: string;
+  jpType: string;
 };
 
-const PokemonThumbnails = ({ id, name, image, type }: Props) => {
+const PokemonThumbnails = ({ id, name, image, type, jpName, jpType }: Props) => {
+  const style = `thumb-container ${type}`;
   return (
-    <div>
+    <div className={style}>
       <div className='number'>
         <small>#0{id}</small>
       </div>
-      <img src={image} alt={name} />
+      <img src={image} alt={name}></img>
+      <img src={monsterBall} alt={name} className='icon-image' />
       <div className='detail-wrapper'>
-        <h4>{name}</h4>
-        <h3>{type}</h3>
+        <h4>{jpName ?? '???'}</h4>
+        <h3>{jpType ?? type}</h3>
       </div>
     </div>
   );
